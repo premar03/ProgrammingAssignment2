b5e8622dfe80069592310af04611fc0edd5a1dfc
premar03 <knuppedup@gmail.com>
2024-11-01 10:22:33 -0400

    Wrote makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+#With a lot of help, I decided to try setting x as a matrix, and "z" to null.
+#changed the "mean" values in original functions to "solve"
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:100,7),3,3)) {
+  z <- NULL 
+  set <- function(y) {
+    x <<- y
+    z <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) z <<- solve
+  getsolve <- function() z
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## Worked backwards from "cachemean", changed "mean" to "solve" using variable z, etc.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x$getsolve()
+  if(!is.null(z)) {
+    return(z)
+  }
+  data <- x$get()
+  z <- solve(data, ...)
+  x$setsolve(s)
+  z
 }
